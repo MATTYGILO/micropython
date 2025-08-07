3b32ac572208da95ab0b6a7e8c96f473c0f002d1
MATTYGILO <chiffchaff.org@gmail.com>
2025-08-07 10:12:20 +0100

    Chat Suggestions

--- ports/esp32/machine_i2s.c
+++ ports/esp32/machine_i2s.c
@@ -69,13 +69,12 @@ typedef enum {
 
 typedef struct _machine_i2s_obj_t {
     mp_obj_base_t base;
-    i2s_port_t i2s_id;
-    i2s_chan_handle_t i2s_chan_handle;
+    i2s_port_t port;
     mp_hal_pin_obj_t sck;
     mp_hal_pin_obj_t ws;
     mp_hal_pin_obj_t sd;
-    i2s_dir_t mode;
-    i2s_data_bit_width_t bits;
+    int8_t mode;
+    i2s_bits_per_sample_t bits;
     format_t format;
     int32_t rate;
     int32_t ibuf;
@@ -86,6 +85,7 @@ typedef struct _machine_i2s_obj_t {
     QueueHandle_t non_blocking_mode_queue;
     TaskHandle_t non_blocking_mode_task;
     dma_buffer_status_t dma_buffer_status;
+    QueueHandle_t i2s_event_queue;
 } machine_i2s_obj_t;
 
 STATIC mp_obj_t machine_i2s_deinit(mp_obj_t self_in);
@@ -101,20 +101,20 @@ STATIC const int8_t i2s_frame_map[NUM_I2S_USER_FORMATS][I2S_RX_FRAME_SIZE_IN_BYT
 };
 
 void machine_i2s_init0() {
-    for (i2s_port_t p = 0; p < I2S_NUM_AUTO; p++) {
+    for (i2s_port_t p = 0; p < I2S_NUM_MAX; p++) {
         MP_STATE_PORT(machine_i2s_obj)[p] = NULL;
     }
 }
 
-STATIC int8_t get_frame_mapping_index(i2s_data_bit_width_t bits, format_t format) {
+STATIC int8_t get_frame_mapping_index(i2s_bits_per_sample_t bits, format_t format) {
     if (format == MONO) {
-        if (bits == I2S_DATA_BIT_WIDTH_16BIT) {
+        if (bits == I2S_BITS_PER_SAMPLE_16BIT) {
             return 0;
         } else { // 32 bits
             return 1;
         }
     } else { // STEREO
-        if (bits == I2S_DATA_BIT_WIDTH_16BIT) {
+        if (bits == I2S_BITS_PER_SAMPLE_16BIT) {
             return 2;
         } else { // 32 bits
             return 3;
@@ -122,32 +122,32 @@ STATIC int8_t get_frame_mapping_index(i2s_data_bit_width_t bits, format_t format
     }
 }
 
-STATIC i2s_data_bit_width_t get_dma_bits(uint8_t mode, i2s_data_bit_width_t bits) {
-    if (mode == MICROPY_PY_MACHINE_I2S_CONSTANT_TX) {
+STATIC i2s_bits_per_sample_t get_dma_bits(uint8_t mode, i2s_bits_per_sample_t bits) {
+    if (mode == (I2S_MODE_MASTER | I2S_MODE_TX)) {
         return bits;
     } else { // Master Rx
         // read 32 bit samples for I2S hardware.  e.g. MEMS microphones
-        return I2S_DATA_BIT_WIDTH_32BIT;
+        return I2S_BITS_PER_SAMPLE_32BIT;
     }
 }
 
-STATIC i2s_slot_mode_t get_dma_format(uint8_t mode, format_t format) {
-    if (mode == MICROPY_PY_MACHINE_I2S_CONSTANT_TX) {
+STATIC i2s_channel_fmt_t get_dma_format(uint8_t mode, format_t format) {
+    if (mode == (I2S_MODE_MASTER | I2S_MODE_TX)) {
         if (format == MONO) {
-            return I2S_SLOT_MODE_MONO;
+            return I2S_CHANNEL_FMT_ONLY_LEFT;
         } else {  // STEREO
-            return I2S_SLOT_MODE_STEREO;
+            return I2S_CHANNEL_FMT_RIGHT_LEFT;
         }
     } else { // Master Rx
         // read stereo frames for all I2S hardware
-        return I2S_SLOT_MODE_STEREO;
+        return I2S_CHANNEL_FMT_RIGHT_LEFT;
     }
 }
 
-STATIC uint32_t get_dma_buf_count(uint8_t mode, i2s_data_bit_width_t bits, format_t format, int32_t ibuf) {
+STATIC uint32_t get_dma_buf_count(uint8_t mode, i2s_bits_per_sample_t bits, format_t format, int32_t ibuf) {
     // calculate how many DMA buffers need to be allocated
     uint32_t dma_frame_size_in_bytes =
-        (get_dma_bits(mode, bits) / 8) * (get_dma_format(mode, format) == I2S_SLOT_MODE_STEREO ? 2: 1);
+        (get_dma_bits(mode, bits) / 8) * (get_dma_format(mode, format) == I2S_CHANNEL_FMT_RIGHT_LEFT ? 2: 1);
 
     uint32_t dma_buf_count = ibuf / (DMA_BUF_LEN_IN_I2S_FRAMES * dma_frame_size_in_bytes);
 
@@ -177,23 +177,27 @@ STATIC uint32_t fill_appbuf_from_dma(machine_i2s_obj_t *self, mp_buffer_info_t *
 
         TickType_t delay;
         if (self->io_mode == ASYNCIO) {
-            delay = 0; // stop i2s_channel_read() operation if DMA memory becomes empty
+            delay = 0; // stop i2s_read() operation if DMA memory becomes empty
         } else {
             delay = portMAX_DELAY;  // block until supplied buffer is filled
         }
 
-        esp_err_t ret = i2s_channel_read(
-            self->i2s_chan_handle,
+        esp_err_t ret = i2s_read(
+            self->port,
             self->transform_buffer,
             num_bytes_requested_from_dma,
             &num_bytes_received_from_dma,
             delay);
 
-        // i2s_channel_read returns ESP_ERR_TIMEOUT when buffer cannot be filled by the timeout delay.
-        if ((self->io_mode != ASYNCIO) ||
-            ((self->io_mode == ASYNCIO) && (ret != ESP_ERR_TIMEOUT))) {
-            check_esp_err(ret);
+        // the following is a workaround for a bug in ESP-IDF v4.4
+        // https://github.com/espressif/esp-idf/issues/8121
+        #if (ESP_IDF_VERSION_MAJOR == 4) && (ESP_IDF_VERSION_MINOR >= 4)
+        if ((delay != portMAX_DELAY) && (ret == ESP_ERR_TIMEOUT)) {
+            ret = ESP_OK;
         }
+        #endif
+
+        check_esp_err(ret);
 
         // process the transform buffer one frame at a time.
         // copy selected bytes from the transform buffer into the user supplied appbuf.
@@ -237,14 +241,14 @@ STATIC size_t copy_appbuf_to_dma(machine_i2s_obj_t *self, mp_buffer_info_t *appb
 
     TickType_t delay;
     if (self->io_mode == ASYNCIO) {
-        delay = 0;  // stop i2s_channel_write() operation if DMA memory becomes full
+        delay = 0;  // stop i2s_write() operation if DMA memory becomes full
     } else {
         delay = portMAX_DELAY;  // block until supplied buffer is emptied
     }
 
-    esp_err_t ret = i2s_channel_write(self->i2s_chan_handle, appbuf->buf, appbuf->len, &num_bytes_written, delay);
+    esp_err_t ret = i2s_write(self->port, appbuf->buf, appbuf->len, &num_bytes_written, delay);
 
-    // i2s_channel_write returns ESP_ERR_TIMEOUT when buffer cannot be emptied by the timeout delay.
+    // i2s_write returns ESP_ERR_TIMEOUT when buffer cannot be emptied by the timeout delay.
     if ((self->io_mode != ASYNCIO) ||
         ((self->io_mode == ASYNCIO) && (ret != ESP_ERR_TIMEOUT))) {
         check_esp_err(ret);
@@ -276,34 +280,6 @@ STATIC void task_for_non_blocking_mode(void *self_in) {
     }
 }
 
-// callback indicating that a DMA buffer was just filled with samples received from an I2S port
-STATIC IRAM_ATTR bool i2s_rx_recv_callback(i2s_chan_handle_t handle, i2s_event_data_t *event, void *self_in) {
-    machine_i2s_obj_t *self = (machine_i2s_obj_t *)self_in;
-    self->dma_buffer_status = DMA_MEMORY_NOT_EMPTY;
-    return false;
-}
-
-// callback indicating that samples in a DMA buffer were just transmitted to an I2S port
-STATIC IRAM_ATTR bool i2s_tx_sent_callback(i2s_chan_handle_t handle, i2s_event_data_t *event, void *self_in) {
-    machine_i2s_obj_t *self = (machine_i2s_obj_t *)self_in;
-    self->dma_buffer_status = DMA_MEMORY_NOT_FULL;
-    return false;
-}
-
-i2s_event_callbacks_t i2s_callbacks = {
-    .on_recv = i2s_rx_recv_callback,
-    .on_recv_q_ovf = NULL,
-    .on_sent = i2s_tx_sent_callback,
-    .on_send_q_ovf = NULL,
-};
-
-i2s_event_callbacks_t i2s_callbacks_null = {
-    .on_recv = NULL,
-    .on_recv_q_ovf = NULL,
-    .on_sent = NULL,
-    .on_send_q_ovf = NULL,
-};
-
 STATIC void mp_machine_i2s_init_helper(machine_i2s_obj_t *self, mp_arg_val_t *args) {
     // are Pins valid?
     int8_t sck = args[ARG_sck].u_obj == MP_OBJ_NULL ? -1 : machine_pin_get_id(args[ARG_sck].u_obj);
@@ -312,15 +288,15 @@ STATIC void mp_machine_i2s_init_helper(machine_i2s_obj_t *self, mp_arg_val_t *ar
 
     // is Mode valid?
     int8_t mode = args[ARG_mode].u_int;
-    if ((mode != (MICROPY_PY_MACHINE_I2S_CONSTANT_RX)) &&
-        (mode != (MICROPY_PY_MACHINE_I2S_CONSTANT_TX))) {
+    if ((mode != (I2S_MODE_MASTER | I2S_MODE_RX)) &&
+        (mode != (I2S_MODE_MASTER | I2S_MODE_TX))) {
         mp_raise_ValueError(MP_ERROR_TEXT("invalid mode"));
     }
 
     // is Bits valid?
-    i2s_data_bit_width_t bits = args[ARG_bits].u_int;
-    if ((bits != I2S_DATA_BIT_WIDTH_16BIT) &&
-        (bits != I2S_DATA_BIT_WIDTH_32BIT)) {
+    i2s_bits_per_sample_t bits = args[ARG_bits].u_int;
+    if ((bits != I2S_BITS_PER_SAMPLE_16BIT) &&
+        (bits != I2S_BITS_PER_SAMPLE_32BIT)) {
         mp_raise_ValueError(MP_ERROR_TEXT("invalid bits"));
     }
 
@@ -357,60 +333,48 @@ STATIC void mp_machine_i2s_init_helper(machine_i2s_obj_t *self, mp_arg_val_t *ar
         self->dma_buffer_status = DMA_MEMORY_NOT_EMPTY;
     }
 
-    i2s_chan_config_t chan_config = I2S_CHANNEL_DEFAULT_CONFIG(self->i2s_id, I2S_ROLE_MASTER);
-    chan_config.dma_desc_num = get_dma_buf_count(mode, bits, format, self->ibuf);
-    chan_config.dma_frame_num = DMA_BUF_LEN_IN_I2S_FRAMES;
-    chan_config.auto_clear = true;
-
-    if (mode == MICROPY_PY_MACHINE_I2S_CONSTANT_TX) {
-        check_esp_err(i2s_new_channel(&chan_config, &self->i2s_chan_handle, NULL));
-    } else { // rx
-        check_esp_err(i2s_new_channel(&chan_config, NULL, &self->i2s_chan_handle));
-    }
-
-    i2s_std_slot_config_t slot_cfg = I2S_STD_PHILIPS_SLOT_DEFAULT_CONFIG(get_dma_bits(mode, bits), get_dma_format(mode, format));
-    slot_cfg.slot_mask = I2S_STD_SLOT_BOTH;
-
-    i2s_std_config_t std_cfg = {
-        .clk_cfg = I2S_STD_CLK_DEFAULT_CONFIG(self->rate),
-        .slot_cfg = slot_cfg,
-        .gpio_cfg = {
-            .mclk = I2S_GPIO_UNUSED,
-            .bclk = self->sck,
-            .ws = self->ws,
-            .invert_flags = {
-                .mclk_inv = false,
-                .bclk_inv = false,
-                .ws_inv = false,
-            },
-        },
+    // Configure I2S using legacy API
+    i2s_config_t i2s_config = {
+        .mode = mode,
+        .sample_rate = self->rate,
+        .bits_per_sample = get_dma_bits(mode, bits),
+        .channel_format = get_dma_format(mode, format),
+        .communication_format = I2S_COMM_FORMAT_STAND_I2S,
+        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
+        .dma_buf_count = get_dma_buf_count(mode, bits, format, self->ibuf),
+        .dma_buf_len = DMA_BUF_LEN_IN_I2S_FRAMES,
+        .use_apll = false,
+        .tx_desc_auto_clear = true,
+        .fixed_mclk = 0,
+        .mclk_multiple = I2S_MCLK_MULTIPLE_256,
+        .bits_per_chan = I2S_BITS_PER_CHAN_DEFAULT,
     };
 
-    if (mode == MICROPY_PY_MACHINE_I2S_CONSTANT_TX) {
-        std_cfg.gpio_cfg.dout = self->sd;
-        std_cfg.gpio_cfg.din = I2S_GPIO_UNUSED;
-    } else { // rx
-        std_cfg.gpio_cfg.dout = I2S_GPIO_UNUSED;
-        std_cfg.gpio_cfg.din = self->sd;
-    }
+    check_esp_err(i2s_driver_install(self->port, &i2s_config, 0, &self->i2s_event_queue));
 
-    check_esp_err(i2s_channel_init_std_mode(self->i2s_chan_handle, &std_cfg));
-    check_esp_err(i2s_channel_register_event_callback(self->i2s_chan_handle, &i2s_callbacks, self));
-    check_esp_err(i2s_channel_enable(self->i2s_chan_handle));
+    i2s_pin_config_t pin_config = {
+        .mck_io_num = I2S_PIN_NO_CHANGE,
+        .bck_io_num = self->sck,
+        .ws_io_num = self->ws,
+        .data_out_num = (mode & I2S_MODE_TX) ? self->sd : I2S_PIN_NO_CHANGE,
+        .data_in_num = (mode & I2S_MODE_RX) ? self->sd : I2S_PIN_NO_CHANGE,
+    };
+
+    check_esp_err(i2s_set_pin(self->port, &pin_config));
 }
 
-STATIC machine_i2s_obj_t *mp_machine_i2s_make_new_instance(mp_int_t i2s_id) {
-    if (i2s_id < 0 || i2s_id >= I2S_NUM_AUTO) {
+STATIC machine_i2s_obj_t *mp_machine_i2s_make_new_instance(mp_int_t port) {
+    if (port < 0 || port >= I2S_NUM_MAX) {
         mp_raise_ValueError(MP_ERROR_TEXT("invalid id"));
     }
 
     machine_i2s_obj_t *self;
-    if (MP_STATE_PORT(machine_i2s_obj)[i2s_id] == NULL) {
+    if (MP_STATE_PORT(machine_i2s_obj)[port] == NULL) {
         self = mp_obj_malloc_with_finaliser(machine_i2s_obj_t, &machine_i2s_type);
-        MP_STATE_PORT(machine_i2s_obj)[i2s_id] = self;
-        self->i2s_id = i2s_id;
+        MP_STATE_PORT(machine_i2s_obj)[port] = self;
+        self->port = port;
     } else {
-        self = MP_STATE_PORT(machine_i2s_obj)[i2s_id];
+        self = MP_STATE_PORT(machine_i2s_obj)[port];
         machine_i2s_deinit(self);
     }
 
@@ -419,12 +383,7 @@ STATIC machine_i2s_obj_t *mp_machine_i2s_make_new_instance(mp_int_t i2s_id) {
 
 STATIC void mp_machine_i2s_deinit(machine_i2s_obj_t *self) {
     if (!self->is_deinit) {
-        if (self->i2s_chan_handle) {
-            i2s_channel_disable(self->i2s_chan_handle);
-            i2s_channel_register_event_callback(self->i2s_chan_handle, &i2s_callbacks_null, self);
-            i2s_del_channel(self->i2s_chan_handle);
-            self->i2s_chan_handle = NULL;
-        }
+        i2s_driver_uninstall(self->port);
 
         if (self->non_blocking_mode_task != NULL) {
             vTaskDelete(self->non_blocking_mode_task);
@@ -470,4 +429,4 @@ STATIC void mp_machine_i2s_irq_update(machine_i2s_obj_t *self) {
     }
 }
 
-MP_REGISTER_ROOT_POINTER(struct _machine_i2s_obj_t *machine_i2s_obj[I2S_NUM_AUTO]);
+MP_REGISTER_ROOT_POINTER(struct _machine_i2s_obj_t *machine_i2s_obj[I2S_NUM_MAX]);
